befc89108429c6cf7a47c9bc2929e02b277f955b
jjjosueva <alexanderjosue192004@gmail.com>
2024-10-30 08:49:28 -0500

    Completo el codigo de Del modelo JValencia con las validaciones y las propiedades

--- JosueValencia_EXAMPRIMERO/Models/JValencia.cs
+++ JosueValencia_EXAMPRIMERO/Models/JValencia.cs
@@ -1,7 +1,32 @@
-﻿namespace JosueValencia_EXAMPRIMERO.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JosueValencia_EXAMPRIMERO.Models
 {
     public class JValencia
     {
+        [Key]
+        public int Id { get; set; }
 
+        [Required]
+        public float Sueldo { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Nombre { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Correo { get; set; }
+
+        public bool ClienteAntiguo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Pedido { get; set; }
+
+        public Celulares Celular { get; set; }
+
+        [ForeignKey("Celular")]
+        public int IdCelular { get; set; }
     }
 }
