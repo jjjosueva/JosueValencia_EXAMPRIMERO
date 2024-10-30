e2967620fba0a70fb06ae052a7a4a116dd1324c1
jjjosueva <alexanderjosue192004@gmail.com>
2024-10-30 08:35:08 -0500

    Primer modelo de celular creado y programado

--- JosueValencia_EXAMPRIMERO/Models/Celulares.cs
+++ JosueValencia_EXAMPRIMERO/Models/Celulares.cs
@@ -1,6 +1,18 @@
-﻿namespace JosueValencia_EXAMPRIMERO.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JosueValencia_EXAMPRIMERO.Models
 {
     public class Celulares
     {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Modelo { get; set; }
+
+        [Range(2000, 2024)]
+        public int Amo { get; set; }
+
+        public float Precio { get; set; }
     }
 }
